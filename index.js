44f4b8831d1f3520416223ddd0aab0293dbf0eeb
Ajay <ajay.ranga.mr.unique@gmail.com>
2022-10-26 13:27:48 +0530

    string rotation task

---
+++ index.js
@@ -0,0 +1,11 @@
+var str = 'hello ajay, nice to meet you ajay, how are you today ajay?';
+
+// var s = 0;
+// str.split('ajay').map((itm) => {
+//   console.log(str.indexOf(itm) + itm.length);
+// });
+
+//console.log(str.split('ajay').join('Mr ajay'));
+
+var arr = [4, 7, 2, 3, 9];
+console.log(arr.sort((a, b) => b - a));
